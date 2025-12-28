1330539588e8ee1d600d8ae9e2db090ee824c81e
anycorea <anycorea@gmail.com>
2025-12-28 10:56:50 +0900

    Update config.py

--- news/config.py
+++ news/config.py
@@ -43,13 +43,13 @@ RSS_SOURCES = [
     ("고용노동부(정책)", "https://www.moel.go.kr/rss/policy.do"),
     # 병원·의료 전문 언론
     ("병원신문", "https://www.khanews.com/rss/allArticle.xml"),
-    ("데일리메디", "https://www.dailymedi.com/rss/allArticle.xml"),
-    ("메디게이트", "https://www.medigatenews.com/rss/allArticle.xml"),
-    ("메디칼타임즈", "http://www.medicaltimes.com/rss/allArticle.xml"),
     ("의학신문", "http://www.bosa.co.kr/rss/allArticle.xml"),
     ("청년의사", "https://www.docdocdoc.co.kr/rss/allArticle.xml"),
     ("의협신문", "https://www.doctorsnews.co.kr/rss/allArticle.xml"),
-    ("라포르시안", "https://www.rapportian.com/rss/allSection.xml"),
+    ("데일리메디", "https://news.google.com/rss/search?q=source:데일리메디+when:7d&hl=ko&gl=KR&ceid=KR:ko"),
+    ("메디게이트", "https://news.google.com/rss/search?q=source:메디게이트뉴스+when:7d&hl=ko&gl=KR&ceid=KR:ko"),
+    ("메디칼타임즈", "https://news.google.com/rss/search?q=source:메디칼타임즈+when:7d&hl=ko&gl=KR&ceid=KR:ko"),
+    ("라포르시안", "https://news.google.com/rss/search?q=source:라포르시안+when:7d&hl=ko&gl=KR&ceid=KR:ko")    
 ]
 
 # ----------------------------
